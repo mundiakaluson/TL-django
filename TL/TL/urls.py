ed2a0e814986f3e337b29a1c2f50af0347ca6c5a
mundia <mundiakaluson@gmail.com>
2021-01-24 01:13:56 +0300

    working on currency options

--- TL/TL/urls.py
+++ TL/TL/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from mainapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('terminal/', admin.site.urls),
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('writers/', include('writers.urls'), name='writers'),
--- TL/mainapp/admin.py
+++ TL/mainapp/admin.py
@@ -4,6 +4,7 @@ from . import models
 class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('topic', 'style', 'words', 'level', 'deadline', 'price')
+    list_filter = ('topic', 'deadline', 'price')
 
 admin.site.register(models.Order, OrderAdmin)
 

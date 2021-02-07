821c0eda6ebf92791bd5b7882bc3aa2ceb49277f
mundia <mundiakaluson@gmail.com>
2021-02-07 15:45:27 +0300

    some more changed

--- TL/TL/settings.py
+++ TL/TL/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,5 +144,3 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
-
-django_heroku.settings(locals())

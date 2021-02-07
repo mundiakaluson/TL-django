51e42e18d36e6b79838b8b9461aca479fe7835f9
mundia <mundiakaluson@gmail.com>
2021-02-07 13:06:16 +0300

    done

--- TL/mainapp/models.py
+++ TL/mainapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 from djmoney.models.fields import MoneyField
+
 class Order(models.Model):
 
     LEVEL = (
